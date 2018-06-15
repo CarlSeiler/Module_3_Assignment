b32543440964ee6f16bcd8b80ec159adcc1b44db
Carl Seiler <cseilergithub@overanalysis.org>
2018-06-15 11:59:10 -0500

    More cleanup

--- migrate-data.js
+++ migrate-data.js
@@ -2,18 +2,14 @@
 const mongoClient = require('mongodb').MongoClient;
 const async = require('async');
 
-process.argv.forEach((val, index, array) => {
-    console.log(`${index} : ${val}`);
-});
 let arg = Number(process.argv[2]);
-    
+
 if ((typeof (arg) === 'number' && (arg % 1) === 0 && arg > 0)) {
     // Parameter probably OK, so let's go with it.
+    // Main code goes here
 } else {
     // Parameter probably not OK, so let's get out of here.
-
     console.log(`Parameter ${process.argv[2]} missing or is not positive integer. Exiting.`);
-
 }
 
 // let tasks = [ ( )];
